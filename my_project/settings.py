bb4c359a6e0c3b3171c82bf4cefc854ab7fcbc44
Thiago Souza <mistersouza@icloud.com>
2023-12-18 10:47:03 +0000

    Update settings, DEBUG to True

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tdn3g5@4(dq$em%0nyxdlrct1%h6ecpi6$5exu^-pttuu_nj)m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '.gitpod.io',
